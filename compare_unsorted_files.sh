84dd21f95b31b7059df054b71bc5fb81e682575e
Elif Alkac <daldalelif88@gmail.com>
2022-06-24 17:03:49 +0200

    compares unsorted files

---
+++ compare_unsorted_files.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+printf "\nCOMMON ROW COUNT\n"
+LC_COLLATE=C wc -l < <(comm -12 <(sort $1) <(sort $2))
+
+printf "\nBEFORE\n"
+wc -l $1
+wc -l $2
+
+LC_COLLATE=C shuf <(comm -23 <(sort $1) <(sort $2)) > $3
+
+printf "\nAFTER\n"
+wc -l $3
